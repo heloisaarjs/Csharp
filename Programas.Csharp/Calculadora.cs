1c8aa23d655b7365d8bce84eeb40320d11e72ad8
Heloisa de Araujo Sousa <helo.sousa78@gmail.com>
2025-01-28 19:40:39 -0300

    calculadora

---
+++ Programas.Csharp/Calculadora.cs
@@ -0,0 +1,11 @@
+    internal class Calculadora{
+        static void Main(string[] args){
+            Console.WriteLine("Qual operação deseja fazer:");
+            Console.WriteLine("1 - Adição");
+            Console.WriteLine("2 - Subtração");
+            Console.WriteLine("3 - Multiplicação");
+            Console.WriteLine("4 - Divisão\n");
+
+            Console.ReadLine();
+        }
+    }
